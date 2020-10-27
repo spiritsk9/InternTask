c5d9360fd6680a5c93fdbde65a6b00eb8dc9cc4e
Saurabh Katkar <spiritsk9@gmail.com>
2020-10-26 17:18:32 -1200

    room db update

---
+++ app/src/main/java/com/example/saurabh/interntask/TasksAdapter.java
@@ -0,0 +1,65 @@
+package com.example.saurabh.interntask;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder> {
+
+    private Context mCtx;
+    private List<Task> taskList;
+
+    public TasksAdapter(Context mCtx, List<Task> taskList) {
+        this.mCtx = mCtx;
+        this.taskList = taskList;
+    }
+
+    @Override
+    public TasksViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mCtx).inflate(R.layout.recyclerview_tasks, parent, false);
+        return new TasksViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TasksViewHolder holder, int position) {
+        Task t = taskList.get(position);
+        holder.textViewTask.setText(t.getTask());
+        holder.textViewDesc.setText(t.getDesc());
+        holder.textViewFinishBy.setText(t.getFinishBy());
+
+        holder.count.setText(String.valueOf(position+1));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+    class TasksViewHolder extends RecyclerView.ViewHolder  {
+
+        TextView textViewStatus, textViewTask, textViewDesc, textViewFinishBy, count;
+
+        public TasksViewHolder(View itemView) {
+            super(itemView);
+
+
+            textViewTask = itemView.findViewById(R.id.textViewTask);
+            textViewDesc = itemView.findViewById(R.id.textViewDesc);
+            textViewFinishBy = itemView.findViewById(R.id.textViewFinishBy);
+            count = itemView.findViewById(R.id.count);
+
+
+
+        }
+
+
+    }
+}
